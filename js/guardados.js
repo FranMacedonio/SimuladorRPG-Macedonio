882d8954e21fe5d33a23369cdbdc890c5af95377
Francisco Macedonio <macedoniofrancisco@hotmail.com>
2021-11-16 16:52:17 -0300

    Personajes guardados funcionando

--- js/guardados.js
+++ js/guardados.js
@@ -7,8 +7,17 @@ $(document).ready(function () {
                                     </div>
                                 </div>`)
     } else{
-
-        
-
+        for (const personaje of personajes){
+            $('.guardados').append(`<div class="glass-card guardado ${personaje.elemento}">
+                                        <h1 class="nombre--personaje">${personaje.nombre}</h1>
+                                        <img src="../media/${personaje.clase}/${personaje.raza}/a.png">
+                                        <div class="content">
+                                            <h3>${personaje.clase} ${personaje.raza}</h3>
+                                            <h4>"De ${personaje.caracter}, ${personaje.historia} y ${personaje.hobby}."</h4>
+                                            <p>Victoria: <strong>${personaje.victorias}</strong> || Derrotas: <strong>${personaje.derrotas}</strong></p>
+                                        </div>
+                                        <i data-name="${personaje.nombre}" class="fas fa-trash-alt"></i>
+                                    </div>`);
+        }
     }
 });
