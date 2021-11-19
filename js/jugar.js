2d520bdf939f27cda982ccd56dfcd6c68656a39a
Francisco Macedonio <macedoniofrancisco@hotmail.com>
2021-11-19 19:07:41 -0300

    Hover en los botones de JUGAR

--- js/jugar.js
+++ js/jugar.js
@@ -1,3 +1,37 @@
 $(document).ready(function () {
     
+
+    // SECTOR BOTONES ---------------------------------------------------------------------------
+    $('.atacar').hover(function () {
+            $('.texto-activo').text('Ataque basico normal');
+            
+        }, function () {
+            $('.texto-activo').text('Elegi una accion');
+        }
+    );
+
+    $('.especial').hover(function () {
+            $('.texto-activo').text('Ataque especial basado en tu elemento');
+            
+        }, function () {
+            $('.texto-activo').text('Elegi una accion');
+        }
+    );
+
+    $('.objetos').hover(function () {
+            $('.texto-activo').text('Abre tu bolsa de objetos');
+            
+        }, function () {
+            $('.texto-activo').text('Elegi una accion');
+        }
+    );
+
+    $('.abandonar').hover(function () {
+            $('.texto-activo').text('Abandonar la pelea');
+            
+        }, function () {
+            $('.texto-activo').text('Elegi una accion');
+        }
+    );
+
 });
