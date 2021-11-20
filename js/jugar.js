0f35ee4219389899b9afe1a0997115be2d24c75e
Francisco Macedonio <macedoniofrancisco@hotmail.com>
2021-11-20 18:30:54 -0300

    Cinematica terminada

--- js/jugar.js
+++ js/jugar.js
@@ -12,22 +12,58 @@ let razaU = jugar[0].raza;
 let elementoU = jugar[0].elemento;
 let usuarioPV = 100;
 let enemigoPV = 100;
-
 // SECTOR DE INICIO -------------------------------------------------------------------------
 
-// personaje left 70
-// enemigo right 70
+    $('.texto').css('width', '93.5%');
     $('.personaje-cartel').hide();
     $('.enemigo-cartel').hide();
-    displayInicial (`Era un dia tranquilo en la vida de ${nombreU}`);
-    $('.btn-textoUno').click(function (e) { 
+    $('.texto-activo').text(`Era un dia tranquilo en la vida de ${nombreU}..`);
+    $('.btn-textoInicio').fadeIn();
+    $('.btn-textoInicio').click(function (e) { 
         e.preventDefault();
-        $('.texto-activo').text(`Hoy habia decidido tomar otro camino hacia la ruta de su trabajo de ${claseU} para cambiar un poco la rutina`);
+        $('.texto-activo').text(`Hoy habia decidido tomar otro camino hacia la ruta de su trabajo de ${claseU} para cambiar un poco la rutina..`);
 
         $('.personaje-juego img').animate({'left': '70px'}, 1500);
-    });
 
-    
+        $('.btn-textoInicio').click(function (e){
+            e.preventDefault();
+            $('.texto-activo').text('Mientras iba caminando se cruzo con una botella en el piso y como debe hacerse la pateo con toda sus fuerzas..');
+            $('.btn-textoInicio').hide();
+
+            setTimeout(() => {
+                $('.texto-activo').text('*Ruido de golpe*');
+                setTimeout(() => {
+                    $('.texto-activo').text('"Que te pasa a vos casi me sacas un ojo con esa botella"');
+                    $('.enemigo-juego img').animate({'right': '70px'}, 1500);
+                    setTimeout(() => {
+                        $('.texto-activo').text('"Ahora vas a ver con quien te metiste.."');
+                        setTimeout(() => {
+                            $('.texto-activo').text(`Al patear la botella ${nombreU} sin querer golpeo a un borracho que estaba durmiendo al lado de un arbol..`);
+                            $('.btn-textoInicioDos').fadeIn();
+                            setTimeout(() => {
+                                $('.personaje-cartel').fadeIn();
+                                $('.enemigo-cartel').fadeIn();
+                            }, 1000)
+
+                            $('.btn-textoInicioDos').click(function (e) { 
+                                e.preventDefault();
+                                $('.texto-activo').text(`A ${nombreU} no le queda otra que hacerle frente al borracho.`);
+                                $('.btn-textoInicioDos').hide();
+                                $('.btn-textoFinal').show();
+
+                                $('.btn-textoFinal').click(function (e) { 
+                                    e.preventDefault();
+                                    $('.btn-textoFinal').hide();
+                                    $('.texto').css('width', '46%');
+                                    $('.texto-activo').text('Elegi una accion');
+                                });
+                            });
+                        }, 3000);
+                    }, 3000);
+                }, 1000);
+            }, 6000);
+        });
+    });
 
 
 // SECTOR BOTONES ---------------------------------------------------------------------------
@@ -133,6 +169,7 @@ function displaySiguiente (texto){
         e.preventDefault();
         $('.btn-textoDos').hide();
         $('.texto').css('width', '46%');
+        $('.texto-activo').text('Elegi una accion');
     });
 }
 
